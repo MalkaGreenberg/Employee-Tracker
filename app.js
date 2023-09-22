cadb31ee0d2b4800cbb0258853657043038b1e70
MalkaGreenberg <malkagreenberg9@gmail.com>
2023-09-21 20:34:03 -0400

    fixed bugs in app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection(
     password: 'malkag',
     database: 'employeeTracker_db'
   },
-  console.log(`Connected to the employeeTracker_db database.`)
+ // console.log(`Connected to the employeeTracker_db database.`)
 );
 
 const choices = [
@@ -27,6 +27,18 @@ const choices = [
     'update an employee role',
 ];
 
+const employeesTable =`SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.departmentName AS department, roles.salary, 
+                            CONCAT(managers.first_name, ' ', managers.last_name) AS manager
+                            FROM employees employee
+                            INNER JOIN roles ON employee.role_id = roles.id 
+                            INNER JOIN departments ON roles.department_id = departments.id
+                            LEFT OUTER JOIN employees managers ON employee.manager_id = managers.id` ;
+
+const rolesTable = `SELECT roles.id, roles.title, departments.departmentName AS department, roles.salary 
+                        FROM roles
+                        INNER JOIN departments ON roles.department_id = departments.id `;
+
+const deparmentsTable = `select departmentName AS department from departments`;
 // prompt the user 
 
 // get the sql query based on chosen option 
@@ -41,16 +53,16 @@ function start() {
     ]).then((answer) => {
         switch (answer.options){
             case 'view all departments':
-                viewTable("select * from departments");
+                viewTable(deparmentsTable);
                 break;
             case 'add a department':
                 addDepartment();
                 break;
             case 'view all roles':
-                viewTable("select * from roles");
+                viewTable(rolesTable);
                 break;
             case 'view all employees':
-                viewTable("select * from employees");
+                viewTable(employeesTable);
                 break;
             case 'add a role':
                 addRole();
@@ -84,7 +96,7 @@ function addDepartment() {
             sql = `INSERT INTO departments (departmentName) VALUES (?);`;
             db.query(sql, addDept, (err, result) => {
                 if (err) throw err;
-                viewTable("select * from departments");
+                viewTable(deparmentsTable);
             }); 
         })
 }
@@ -120,7 +132,7 @@ function addRole() {
                 sql = `INSERT INTO roles (title,salary,department_id) VALUES (?,?,?);`;
                 db.query(sql, [name, salary, result[0].id], (err) => {
                     if (err) throw err;
-                    viewTable("select * from roles");
+                    viewTable(rolesTable);
                 }); 
             });
             
@@ -141,6 +153,7 @@ function addEmployee() {
             if (err) throw err;
             //collecting the list of employees to choose a manager
             managers = result.map(employees => employees.first_name + ' ' + employees.last_name);
+            managers.push('null');
         //prompt the user
         inquirer
         .prompt([
@@ -176,13 +189,17 @@ function addEmployee() {
                 // getting back the id for the chosen manager
                 db.query('select id from employees where first_name = ? and last_name = ?',[managerName[0], managerName[1]], (err, result) => {
                     if (err) throw err;
-                    const managerId = result[0].id;
-                    console.log(result);
+                    var managerId;
+                    if(managerName[0] == 'null'){
+                        managerId = null;
+                    }else{
+                        managerId = result[0].id;
+                    }
                     sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);`;
                     //adding the employee to the table
                     db.query(sql, [fName, lName, role, managerId], (err) => {
                         if (err) throw err;
-                        viewTable("select * from employees");
+                        viewTable(employeesTable);
                     }); 
                 });
             });
@@ -231,7 +248,7 @@ function updateEmployee() {
                             //adding the employee to the table
                             db.query(sql, [roleId, employeeId], (err) => {
                                 if (err) throw err;
-                                viewTable("select * from employees");
+                                viewTable(employeesTable);
                             }); 
                         });
                     });
@@ -242,14 +259,15 @@ function updateEmployee() {
 
 db.connect((error) => {
     if(error) throw error;
-    console.log(`\x1b[38;5;13m ==========================\x1b[38;5;255m=============================\x1b[38;5;13m===========================
+    console.log(`\x1b[38;5;13m ==========================\x1b[38;5;255m===========================
         
         
         `);
-    console.log(`\x1b[38;5;13m ${f.textSync('Employee Tracker')}`);
+    console.log(`\x1b[38;5;13m ${f.textSync('Employee')}`);
+    console.log(`\x1b[38;5;13m ${f.textSync(`  Tracker`)}`);
     console.log(`\x1b[38;5;13m 
         
- ==========================\x1b[38;5;255m=============================\x1b[38;5;13m=========================== `);
+ ==========================\x1b[38;5;255m=========================== `);
     start();
 });
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,13 +5,13 @@ VALUES ("sales"),
         ("customer service");
 
 INSERT INTO roles (title,salary,department_id)
-VALUES  ("sales Rep", 40.00, 1),
-        ("support Rep", 50.00, 4),
-        ("tech support", 30.00, 3),
-        ("rep", 45.00, 4),
-        ("Advanced support", 65.00, 2);
+VALUES  ("sales Rep", 4000, 1),
+        ("support Rep", 5000, 4),
+        ("tech support", 3000, 3),
+        ("rep", 4500, 4),
+        ("Advanced support", 6500, 2);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES  ("mike", "jackson", 2, NULL),
-        ("jake", "Roberto", 4, 1),
+        ("jake", "Rifkin", 4, 1),
         ("bob", "Marks", 1, 1);
